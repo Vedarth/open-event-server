bf2362f8e1d744ddcbff7df7118cea7199aab11f
Vedarth <vedarth.sharma@gmail.com>
2018-08-01 14:24:14 +0530

    Added migration revision

---
+++ migrations/versions/397a05a9bd1b_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 397a05a9bd1b
+Revises: c83ee94c3cf6
+Create Date: 2018-08-01 12:22:00.844089
+
+"""
+
+from alembic import op
+import sqlalchemy as sa
+import sqlalchemy_utils
+
+
+# revision identifiers, used by Alembic.
+revision = '397a05a9bd1b'
+down_revision = 'c83ee94c3cf6'
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
---
+++ migrations/versions/c83ee94c3cf6_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: c83ee94c3cf6
+Revises: f375453ca1f6
+Create Date: 2018-08-01 12:21:57.665188
+
+"""
+
+from alembic import op
+import sqlalchemy as sa
+import sqlalchemy_utils
+
+
+# revision identifiers, used by Alembic.
+revision = 'c83ee94c3cf6'
+down_revision = 'f375453ca1f6'
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
